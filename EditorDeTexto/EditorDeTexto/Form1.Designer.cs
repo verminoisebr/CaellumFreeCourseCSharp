bdab5739a7727b1bc03331b8d1543c5e0275d847
Marcelo Boschi <marceloboschi78@hotmail.com>
2019-04-10 10:00:35 -0300

    Working with IO Stream, StreamReader

---
+++ EditorDeTexto/EditorDeTexto/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EditorDeTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textoConteudo = new System.Windows.Forms.TextBox();
+            this.ButtonGrava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textoConteudo
+            // 
+            this.textoConteudo.Location = new System.Drawing.Point(62, 25);
+            this.textoConteudo.Multiline = true;
+            this.textoConteudo.Name = "textoConteudo";
+            this.textoConteudo.Size = new System.Drawing.Size(199, 239);
+            this.textoConteudo.TabIndex = 0;
+            // 
+            // ButtonGrava
+            // 
+            this.ButtonGrava.Location = new System.Drawing.Point(62, 293);
+            this.ButtonGrava.Name = "ButtonGrava";
+            this.ButtonGrava.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGrava.TabIndex = 1;
+            this.ButtonGrava.Text = "Gravar";
+            this.ButtonGrava.UseVisualStyleBackColor = true;
+            this.ButtonGrava.Click += new System.EventHandler(this.ButtonGrava_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonGrava);
+            this.Controls.Add(this.textoConteudo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textoConteudo;
+        private System.Windows.Forms.Button ButtonGrava;
+    }
+}
+
---
+++ EditorDeTexto/EditorDeTexto/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EditorDeTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("texto.txt"))
+            {
+                using (Stream entrada = File.Open("texto.txt", FileMode.Open))
+                using (StreamReader leitor = new StreamReader(entrada))
+                {
+                    //using ReadLine()
+                    string linha = leitor.ReadLine();
+                    while (linha != null)
+                    {
+                        textoConteudo.Text += linha;
+                        linha = leitor.ReadLine();
+                    }
+                }
+                // using ReadToEnd() unless ReadLine()
+                /*string linhaCompleta = leitor.ReadToEnd();
+                if (linhaCompleta != null)
+                {
+                    textoConteudo.Text += linhaCompleta;
+                }*/
+                
+                /*leitor.Close();
+                entrada.Close();*/ // this case only when not using "using"
+            }
+        }
+
+        private void ButtonGrava_Click(object sender, EventArgs e)
+        {
+            // in this case no using "using", so the stream and StreamWriter need to be closed
+            Stream saida = File.Open("texto.txt", FileMode.Create);
+            StreamWriter escritor = new StreamWriter(saida);
+            escritor.Write(textoConteudo.Text);
+            escritor.Close();
+            saida.Close();
+        }
+    }
+}
